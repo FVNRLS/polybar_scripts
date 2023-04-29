552bfccddcb16433e54102789e5c5e55860f4902
Roman Mazuritskiy <88487425+FVNRLS@users.noreply.github.com>
2023-04-30 01:26:43 +0200

    Update network_manager.sh
    script performance improved

--- network_manager/network_manager.sh
+++ network_manager/network_manager.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-WIRELESS_INTERFACE=$(ip link | awk -F: '/wlp/ {print $2;getline}' | sed 's/ //')
-WIRELESS_STATE=$(ip link show "$WIRELESS_INTERFACE" | grep "state UP" > /dev/null 2>&1; echo $?)
-ETHERNET_INTERFACE=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | grep "en" | head -1 | sed 's/ //')
+# Cache the results for 60 seconds
+CACHE_EXPIRY=60
+CACHE_FILE="/tmp/network_speed_cache"
 
-if [[ -z $ETHERNET_INTERFACE ]]; then
-  ETHERNET_EXISTS=1
+if [[ -f $CACHE_FILE ]] && [[ $(find "$CACHE_FILE" -mmin -$CACHE_EXPIRY) ]]; then
+  # Use cached results
+  ICON=$(cat "$CACHE_FILE")
 else
+  # Obtain network interface information
+  WIRELESS_INTERFACE=$(ip link | awk -F: '/wlp/ {print $2;getline}' | sed 's/ //')
+  WIRELESS_STATE=$(ip link show "$WIRELESS_INTERFACE" | grep "state UP" > /dev/null 2>&1; echo $?)
+  ETHERNET_INTERFACE=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | grep "en" | head -1 | sed 's/ //')
   ETHERNET_EXISTS=$(ip link show "$ETHERNET_INTERFACE" > /dev/null 2>&1; echo $?)
-fi
 
-if [[ $WIRELESS_STATE -ne 0 && $ETHERNET_EXISTS -ne 0 ]]; then
-  ICON="  "
-elif [[ $WIRELESS_STATE -eq 0 || $ETHERNET_EXISTS -eq 0 ]]; then
-  if [[ -z $ETHERNET_INTERFACE ]]; then
-    ICON="  "
+  # Determine the network speed
+  if [[ $ETHERNET_EXISTS -eq 0 ]]; then
+    SPEED=$(ethtool "$ETHERNET_INTERFACE" | awk '/Speed:/ {print $2}')
+    ICON=" $SPEED"
+  elif [[ $WIRELESS_STATE -eq 0 ]]; then
+    SIGNAL=$(grep "$WIRELESS_INTERFACE" /proc/net/wireless | awk '{ print int($3 * 100 / 70) }')
+    ICON=" $SIGNAL%"
   else
-    ETHERNET_STATE=$(ip link show "$ETHERNET_INTERFACE" | grep "state UP" > /dev/null 2>&1; echo $?)
-
-    if [[ $ETHERNET_STATE -eq 0 ]]; then
-      ICON="  "
-    elif [[ $WIRELESS_STATE -eq 0 ]]; then
-      ICON="  "
-    fi
+    ICON=""
   fi
+
+  # Cache the results
+  echo "$ICON" > "$CACHE_FILE"
 fi
 
-echo "${ICON}"
+echo "$ICON"
